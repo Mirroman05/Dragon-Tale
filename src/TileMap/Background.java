04ca213358c3171d2fbab1f9ee0cdc1abe8aa3af
Tim White <timothy.white@TWhite-C30.hps.com>
2013-11-29 17:38:05 -0500

    GamePanel and StateManager

---
+++ src/TileMap/Background.java
@@ -0,0 +1,16 @@
+package TileMap;
+
+import java.awt.image.BufferedImage;
+
+public class Background {
+
+	private BufferedImage image;
+	
+	private double x;
+	private double y;
+	private double dx;
+	private double dy;
+	
+	private double moveScale;
+	
+}
